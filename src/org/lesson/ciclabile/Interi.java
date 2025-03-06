f2fd7521986a76816d68cf9939489e9af7ae7435
SimoneChiodo <simochi.005@gmail.com>
2025-03-06 15:56:26 +0100

    Add empty constructor, addElemento()

--- src/org/lesson/ciclabile/Interi.java
+++ src/org/lesson/ciclabile/Interi.java
@@ -2,19 +2,40 @@ package org.lesson.ciclabile;
 
 public class Interi {
   
-  int[] array;
+  private int[] array = null;
 
   // Constructors ---------------
-  
+
+  Interi(){
+    
+  }
+
   Interi(int[] array){
     this.array = array;
   }
 
+  
   // Functions ---------------
 
+  public void addElemento(int newNumber){
+    //Create a new array
+    int[] newArray = new int[array.length+1];
+
+    //Copy the old array in the new one
+    for(int i = 0; i < array.length; i++){
+      newArray[i] = array[i];
+    }
+
+    //Add the new number
+    newArray[array.length-1] = newNumber;
+
+    //Overwrite the new array in the old one
+    array = newArray;
+  }
+  
   int indexElementoSuccessivo = 0;
   // Return a piece of this.array on each call
-  int getElementoSuccessivo(){
+  public int getElementoSuccessivo(){
     // Reset the index if it's out the bound
     if(indexElementoSuccessivo == this.array.length)
       indexElementoSuccessivo = 0;
@@ -24,7 +45,7 @@ public class Interi {
   }
 
   // Check if I have already scrolled the entire this.array
-  boolean hasAncoraElementi(){
+  public boolean hasAncoraElementi(){
     if(indexElementoSuccessivo != this.array.length)
       return true;
 
